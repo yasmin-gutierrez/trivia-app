570afa4cf1f4dddf17c23920bdb799f4adc23657
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-03-07 19:26:10 -0600

    added style to categories

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -14,7 +14,14 @@ onMounted(async () => {
 
 <template>
   <BaseTitle>TRIVIA APPLICATION</BaseTitle> 
-  <div v-for="category in categories" :key="category.id">
-    {{ category.name }} 
+  <div class="grid flex-grow grid-cols-4 gap-12 m-20"> 
+    <RouterLink v-for="category in categories" 
+    :key="category.id"
+    :to="''"
+    class="bg-white text-center flex h-32 items-center justify-center rounded-lg border-4 border-slate-500 py-4 font-bold uppercase hover:cursor-pointer hover:border-red-500 hover:bg-red-400 hover:text-white transition-colors duration-300"
+    >
+      {{ category.name }} 
+    </RouterLink>
   </div>
+  
 </template>
